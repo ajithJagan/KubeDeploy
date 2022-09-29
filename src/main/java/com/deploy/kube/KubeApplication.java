3c7e32c2263f0a73b5c3fc8210cf5e2c7450f251
ajiqbrainx <94741631+ajiqbrainx@users.noreply.github.com>
2022-09-29 12:03:25 +0530

    Add

---
+++ src/main/java/com/deploy/kube/KubeApplication.java
@@ -0,0 +1,14 @@
+package com.deploy.kube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubeApplication.class, args);
+		System.out.println("Its Running....");
+	}
+
+}

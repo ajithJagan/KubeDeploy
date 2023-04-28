b2c2024de014012114927a1b4451e40c5e42027c
ajithJagan <94741631+ajithJagan@users.noreply.github.com>
2023-04-28 16:41:43 +0530

    Update ResourceplanningApplicationTests.java

--- src/test/java/com/flexcub/resourceplanning/ResourceplanningApplicationTests.java
+++ src/test/java/com/flexcub/resourceplanning/ResourceplanningApplicationTests.java
@@ -1,13 +1,13 @@
-package com.flexcub.resourceplanning;
+// package com.flexcub.resourceplanning;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ResourceplanningApplicationTests {
+// @SpringBootTest
+// class ResourceplanningApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//     @Test
+//     void contextLoads() {
+//     }
 
-}
+// }
